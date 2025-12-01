8ad125aaf19f5f0e92f355298050f5c1159cd1a7
Aneeshie <aneeshdas556@gmail.com>
2025-12-01 23:08:04 +0530

    feat: added packages and setup eza and zoxide

--- home/packages.nix
+++ home/packages.nix
@@ -1,12 +1,35 @@
 {config, pkgs, ...}:
 
 {
-    home.packages = with pkgs; [
-        git 
-        nodejs 
-        python3
-        curl
-        wget 
-        ripgrep
-    ];
+
+  home.packages = with pkgs; [
+    git
+      vim
+      bash
+      zsh
+      cloc
+      entr
+      eza
+      fd
+      fnm
+      gum
+      fzf
+      gh
+      glow
+      gnupg
+      highlight
+      btop
+      jq
+      lazygit
+      python3
+      ripgrep
+      shellcheck
+      stylua
+      tmux
+      tree
+      wdiff
+      wget
+      zoxide
+      sesh
+      ];
 }
--- home/shell.nix
+++ home/shell.nix
@@ -3,6 +3,9 @@
 {
   programs.zsh = {
     enable = true;
+    initExtra = ''
+      eval "$(zoxide init zsh)"
+    '';
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
@@ -10,7 +13,13 @@
       hm = "home-manager switch --flake ~/.config/home-manager";
       devfs = "nix develop ~/.config/home-manager/dev-envs/fullstack";
       devdsa = "nix develop ~/.config/home-manager/dev-envs/dsa";
+      ls = "eza";
+      l = "eza -l";
+      la = "eza -la";
+      lt = "eza --tree";
+      cd = "z";
     };
+
   };
 }
 
