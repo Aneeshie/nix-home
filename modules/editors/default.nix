5c1d52caa513d755e950f50e2ad29f7825cf3f62
Aneeshie <aneeshdas556@gmail.com>
2026-05-09 16:49:15 +0530

    fix(editors): update neovim config hash for telescope master branch

--- modules/editors/default.nix
+++ modules/editors/default.nix
@@ -9,4 +9,10 @@
     defaultEditor = true; # Replaces EDITOR="emacs" or "vim" in session variables
   };
 
+  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
+    owner = "Aneeshie";
+    repo = "nvim";
+    rev = "0cc89ce58c10b8e04d4db2565d3abe96d75914d8";
+    sha256 = "1qjrcps91jnl4kqhx21x8fgmvlf0yp7zhqvbgl7k2dj4h6gqv1pp";
+  };
 }
